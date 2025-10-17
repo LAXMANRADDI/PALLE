47329a5b4cc2fb5208c565d4e1189b5226eb9b4c
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-10-17 20:45:44 +0530

    Update ternary.py

--- ternary.py
+++ ternary.py
@@ -16,3 +16,20 @@ print (res)
 x = int ( input ('entr age: ') )
 res = {False :'is not adult ' , True :'is adult '}[x >= 18]
 print (res)
+
+
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+simple use of if condn
+
+a =[]
+count=0
+for i in range(0,11,2):
+    a.append(i)
+#print (a) 
+
+for i in a :
+    if i%2 == 0 :
+        count+=1
+        
+if count >= 5:
+    print ( ' limt reached')
