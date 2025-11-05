d00d1959452a265d1a5c1f3422605bcfd0df66c7
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-05 21:58:44 +0530

    Update biuiltinfun.py

--- biuiltinfun.py
+++ biuiltinfun.py
@@ -1,9 +1,9 @@
 creatinga timer using simple while loop 
 
-import time
-x = input (int ())
+import time # import time from sys
+x = input (int ()) # time in seconds 
 while x>= 1:
   print(x)
-  time.sleep(1 , flush ==True)
-  x -= 1
-print (" go on ")
+  time.sleep(1 , flush ==True) # sleep is used create delay in printng ouput  and flush here is not recomended , is used to print output immidiatly 
+  x -= 1 # decrement
+print (" time up ")
