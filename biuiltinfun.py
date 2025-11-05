e978315a9047282a888f1eec6b97c84eb1edeced
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-05 22:10:28 +0530

    Update biuiltinfun.py

--- biuiltinfun.py
+++ biuiltinfun.py
@@ -7,3 +7,11 @@ while x>= 1:
   time.sleep(1 , flush ==True) # sleep is used create delay in printng ouput  and flush here is not recomended , is used to print output immidiatly 
   x -= 1 # decrement
 print (" time up ")
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+some use cases of split() function
+s = 'palle technology'
+r =  s.split() #   [ 'palle' , 'technology']
+r= s.spit('') # throws error since seperator is not defined
+r = s.split('e', 1) # ['pall' , ' technology']
+r= s.split('e', 2) # ['pall' , ' t' , 'chnology']
