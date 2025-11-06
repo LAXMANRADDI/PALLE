fb949857af87697349667b69b4985ffb9d02ee0f
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-06 21:52:04 +0530

    Update exam.py

--- exam.py
+++ exam.py
@@ -62,6 +62,15 @@ if n == s:
 else:
     print('Number is not a palindrome')
 
+$$$$$$$$$$$$$$$$$$$$$$$
+def isPalindrome(x: int) -> bool:
+    if x < 0 or (x % 10 == 0 and x != 0):
+        return False
+    reversed_num = 0
+    while x > reversed_num:
+        reversed_num = reversed_num * 10 + x % 10
+        x //= 10
+    return x == reversed_num or x == reversed_num // 10
 
 
 
