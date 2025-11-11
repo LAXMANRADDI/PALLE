e502eeffa75108c5ca6cdb947443bf2f7f674bdc
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-11 23:07:53 +0530

    Update and rename biuiltinfun.py to methds.py

--- methds.py
+++ methds.py
@@ -18,3 +18,16 @@ r= s.split('e', 2) # ['pall' , ' t' , 'chnology']
 r= s.split ('l') # ['pa', '', 'e techno', 'ogy']
 
 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+practice problems related to ascii conversions 
+*] simple problem related to conversion of given charectors in list to another case and vice versa 
+l1 = ['a', 'A', 'c', 'G']
+l2 = []
+
+for i in l1:
+    if 'A' <= i <= 'Z':        # uppercase → lowercase
+        l2.append(chr(ord(i) + 32))
+    elif 'a' <= i <= 'z':      # lowercase → uppercase
+        l2.append(chr(ord(i) - 32))
+
+print(l2)
