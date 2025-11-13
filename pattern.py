d8c5b7774b6328226e67da2e3a66b745bcfa1316
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-13 22:02:27 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -40,3 +40,23 @@ logic:
     for i in range(1,5):
     print(' '.join(str(i)* i)) 
 
+
+3]   1
+     2 3
+     4 5 6
+     7 8 9 10
+
+3.1] n = 4
+     num = 1
+     for i in range(1, n + 1):
+         for j in range(1, i + 1):
+            print(num, end=" ")
+            num += 1
+         print() 
+3.2 ] 
+n = 5
+num = 1
+for i in range(1, n + 1):
+    row = [str(num + j) for j in range(i)]
+    print(' '.join(row))
+    num += i
