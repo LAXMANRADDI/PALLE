3dc80122a535c304edc1e64226557e22a41773ae
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-13 22:27:31 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -66,18 +66,13 @@ required
 1 2 3 
 1 2 
 1  
-i]num = 6
-for i in range(1,num +1):
-    for j in range( 1 , num -i ):
-        print(j, end =' ')
-    print()
 
-ii]n = 4
+i]n = 4
 for i in range(n, 0, -1):
     for j in range(1, i + 1):
         print(j, end=" ")
     print()
-iii]
+ii]
 n = 4
 for i in range(n, 0, -1):
     print(' '.join(str(j) for j in range(1, i + 1)))
