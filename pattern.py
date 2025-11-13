95dc570d2bf736ebe0bccec7558c09cdc0538f4a
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-13 11:39:52 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -47,3 +47,10 @@ i	Expression	Result
 1 2
 1 2 3
 1 2 3 4
+
+_---------------------------------------------------
+n = 4
+for i in range(1, n + 1):           # Outer loop → rows
+    for j in range(1, i + 1):       # Inner loop → numbers in each row
+        print(j, end=" ")           # Print numbers in the same line
+    print()                         # Move to the next line after each row
