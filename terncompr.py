0f48154191c5d0b911a0c998dd500afe56ea47f5
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-16 23:39:05 +0530

    Update terncompr.py

--- terncompr.py
+++ terncompr.py
@@ -1,7 +1,7 @@
 ternanry condition for checking greatest of three numbres 
 a ,b ,c =10 ,20,30
 print (f'{a} is largest ' if a>b and a>c else(f'{b} is greater' if b>c  else f' {c} is largest ')) 
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+----------------------------------------------------------
    trenary with  help of tuples 
 
 x = int ( input ('entr age: ') )
